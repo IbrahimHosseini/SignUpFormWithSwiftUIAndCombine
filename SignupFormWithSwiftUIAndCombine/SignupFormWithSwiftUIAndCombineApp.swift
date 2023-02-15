5211389b110535f3335ece01c64778b7276b85b6
Ebrahim Hosseini <sehosseini@me.com>
2023-02-16 01:20:15 +0330

    Initial Commit

---
+++ SignupFormWithSwiftUIAndCombine/SignupFormWithSwiftUIAndCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  SignupFormWithSwiftUIAndCombineApp.swift
+//  SignupFormWithSwiftUIAndCombine
+//
+//  Created by Ibrahim Hosseini on 2/16/23.
+//
+
+import SwiftUI
+
+@main
+struct SignupFormWithSwiftUIAndCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
